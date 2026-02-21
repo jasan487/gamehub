3f83a0369ebf47de42b02dbf20515df8a52cad64
{name} <you@example.com>
2026-02-21 15:38:15 +0530

    Fetch the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,5 +1,14 @@
+import useGenres from "../hooks/useGenres";
+
 const GenreList = () => {
-  return <div>GenreList</div>;
+  const { genres } = useGenres();
+  return (
+    <ul>
+      {genres.map((genre) => (
+        <li key={genre.id}>{genre.name}</li>
+      ))}
+    </ul>
+  );
 };
 
 export default GenreList;
