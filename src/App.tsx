abbbfda784acc2c2c48602e8ccebaa15d8e90019
{name} <you@example.com>
2026-02-21 11:06:19 +0530

    install chakra ui

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import NavBar from "./components/NavBar";
+
+const App = () => {
+  return <NavBar />;
+};
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { ChakraBaseProvider } from "@chakra-ui/react";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ChakraBaseProvider>
+      <App />
+    </ChakraBaseProvider>
+  </StrictMode>,
+);
