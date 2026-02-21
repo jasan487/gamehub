3e1c7d11ae8eeafd6f072ea2cb58316fe07f8d02
{name} <you@example.com>
2026-02-21 12:18:49 +0530

    create custom hook to fetch api

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import GameGrid from "./components/GameGrid";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
-// import NavBar from "./components/NavBar";
+import GameGrid from "./components/GameGrid";
 
 const App = () => {
   return (
-    <>
-      <NavBar />
-      <GameGrid />
-    </>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar />
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside">Aside</GridItem>
+      </Show>
+      <GridItem area="main">
+        <GameGrid />
+      </GridItem>
+    </Grid>
   );
 };
 
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { AxiosError, CanceledError } from "axios";
+
+interface Game {
+  id: number;
+  name: string;
+}
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const controller = new AbortController();
+    apiClient
+      .get<FetchGamesResponse>("/games", { signal: controller.signal })
+      .then((res) => setGames(res.data.results))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        const error = err as AxiosError;
+
+        if (!error.response) {
+          setError("Network Error - Check your internet or API key");
+        } else {
+          setError(error.message);
+        }
+      });
+
+    return () => controller.abort();
+  }, []);
+  return { games, error };
+};
+
+export default useGames;
